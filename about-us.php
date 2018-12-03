fcdf73be34a62c4861ffba49977a14760bd3088b
rajeev <rajeevkashyap002@gmail.com>
2018-12-04 00:29:30 +0530

    about us add

---
+++ about-us.php
@@ -0,0 +1,43 @@
+<?php include('header.php');?>
+<!-- header end -->
+
+<!-- Start Bottom Header -->
+<div class="header-bg page-area">
+<div class="home-overly"></div>
+<div class="fluid-container">
+<div class="row">
+<div class="col-md-12 col-sm-12 col-xs-12">
+<img src="img/slider/About Us-Home.jpg" alt="" title="#slider-direction-1" style="width:1920px; height:420px;" />
+</div>
+</div>
+</div>
+</div>
+<!-- END Header -->
+
+<div class="container">
+<div>
+	<Br>
+	<h3>DELIVERING PERFECTION IN SERVICES BEYOND YOUR EXPECTATION!</h3>
+<p style="text-align: justify;">
+Visit Advertiser is a new invention leading SEO and Digital marketing firm based in Delhi NCR providing their services to both B2B and B2C business enterprises having different domains. Visit advertiser is known as the best SEO agency in Delhi NCR, as we provide the marvellous SEO services and give your online portal a top rank on search engines. We offer supple services with reasonable price tags to deal with the online businesses of our worthy consumers in the most efficient way.</p>
+
+<p style="text-align: justify;">With the latest SEO tools, tenable internet connection, superior communication channels and 24/7 client service; Visit advertiser offers a trustworthy solutions in various domains such as business website designing service, e-commerce websites designing, link building services, search engine optimization or SEO services, Digital marketing services, e-mail marketing services, brand management services, website development services, mobile application development and business consulting.</p>
+
+<br>
+<h3>Our Team </h3>
+<p style="text-align: justify;">We have a young, zealous and enthusiastic team of professionals. Each individual is talented in the specific skills to attain proficiency in our work. Our passionate team has both fresher and qualified professionals so as to hold up the sustainable development in all the technical services offered by us. We have marvellous website developers, creative website designers, search engine optimization professionals, online marketers, passionate software developers and many application developers in our proficient team, who deliberately work in partnership with each other for offering the unsurpassed solutions to our clientele.</p>
+
+<h3>Our Mission</h3>
+<p style="text-align: justify;">Our team totally focus on step up the industry development of our worthy clientele by applying the most recent industrial tools. At Visit Advertiser, our main vision is to facilitate other business firms to attain organizational achievement through lead generation, increased revenue, get top rank on Google ladders, cost management, and excellence in other services.</p>
+
+
+<h3>Why Us?</h3>
+<p style="text-align: justify;">Visit Advertiser is a business forthcoming industry which offers the full assortment of promotional services which may assist any industry to flourish and grow up in the market competition. We have the capability to help you to run your business firm in desired and optimistic environment. Several other corporates from diverse industries have assigned Visit Advertiser with the task of managing their SEO services and Digital marketing services.</p>
+
+</div>
+</div>
+<br>
+<!-- End Blog Area -->
+<div class="clearfix"></div>
+<!-- Start Footer bottom Area-->
+<?php include('footer.php');?>
